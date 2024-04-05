bb8f59b842aa9a8274b3678b774f6fd8105f0d73
Ryo Yanagida <Ryo.Yanagida@glasgow.ac.uk>
2024-04-05 14:23:17 +0100

    WIP QoS mechanism draft

---
+++ daemon/table/qos_table.hpp
@@ -0,0 +1,37 @@
+#ifndef NFD_DAEMON_TABLE_QOS_TABLE
+#define NFD_DAEMON_TABLE_QOS_TABLE
+
+#include <vector>
+
+#include "daemon/qos/qos-entry.hpp"
+
+namespace nfd {
+namespace qos {
+  class QosTable : noncopyable
+  {
+  public:
+    explicit
+    QosTable();
+
+  public:
+    void
+    push(const QosEntry entry);
+
+    bool 
+    erase(QosEntry& entry);
+
+    uint16_t
+    match_and_fetch(ndn::Name& name);
+
+
+    std::vector<QosEntry> m_policy_vector;
+
+  private:
+    QosEntry
+    const get(Name& name);
+
+  };
+}
+}
+
+#endif // NFD_DAEMON_TABLE_QOS_TABLE
